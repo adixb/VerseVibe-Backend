5188d7fe90c809a0d0f8e53a9ffb30304aaedb74
adixb <aditya.bhattacharjee706@gmail.com>
2024-09-12 22:36:56 +0530

    backend project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express') ; 
+const cors = require('cors')
+const dotenv = require('dotenv') 
+
+dotenv.config() ; 
+
+const app = express() ; 
+const PORT_NUMBER = process.env.PORT || 8000
+
+app.use(cors({
+    origin:["https://localhost:3000/"],
+    methods:["POST","GET","PUT","DELETE"],
+    credentials:true,
+})) ; 
+app.use(express.urlencoded({extended:true})) ; 
+
+app.listen(PORT_NUMBER,()=>{console.log(`Server is running on ${PORT_NUMBER}`)}) ;
+
