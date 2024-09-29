c07b9e0317c3177d9eee6b32d62d516c7b6108bb
adixb <aditya.bhattacharjee706@gmail.com>
2024-09-29 12:51:10 +0530

    updated code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT_NUMBER = process.env.PORT || 8000;
 
 // Middlewares
 app.use(cors({
-    origin: 'https://verse-vibe-smoky.vercel.app', 
+    origin: 'https://verse-vibe-smoky.vercel.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, 
 }));
